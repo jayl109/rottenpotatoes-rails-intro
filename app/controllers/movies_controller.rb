32f9826e8e9fa6b2fb6670a2f0570e6ca51363a8
jason lum <jthemathfreak@gmail.com>
2017-09-21 18:25:34 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,19 +14,63 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.ratings()
-    puts "showing"
-    if (params[:sort] != nil)
-      print("here")
-      @movies = Movie.order(params[:sort])
-    else
-      if (params[:ratings] != nil)
-        @movies = Movie.where("rating = ?", params[:ratings].keys)
-      else 
-        @movies = Movie.all
-      end
     
+    #@movies = Movie.all
+
+    if (params[:sort] != nil)
+      if (session[:sort] == params[:sort])
+        if (params[:redirected] == nil)
+          session.delete(:sort)
+        end
+      else
+        session[:sort] = params[:sort]
+      end
+      
     end
+    if (params[:ratings] != nil)
+        session[:ratings] = params[:ratings]
+    end
+    if (session[:ratings] != nil && params[:ratings] == nil)
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings], :redirected => true)
+    end
+     if (session[:sort] != nil && params[:sort] == nil)
+      flash.keep
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings], :redirected => true)
+    end
+    if (session[:ratings] == nil)
+      @movies = Movie.all
+    else
+      @movies = Movie.where(rating: session[:ratings].keys)
+    end
+    if (session[:sort] != nil) 
+     @movies = @movies.order(session[:sort])
+    end
+    @all_ratings = {}
+    Movie.ratings().each do |rating|
+      val = (session[:ratings] == nil) 
+      val2 = (session[:ratings].include? rating)
+      puts(val)
+      puts(val2)
+      @all_ratings[rating.to_sym()] = val || val2
+    end
+    puts(@all_ratings)
+    #if (session[:ratings] != nil)
+     # s = "rating in ("
+      #i = 1
+      #session[:ratings].each do |key, value| 
+       # s = s + key
+      #  if (i != session[:ratings].count)
+       #   s = s + ", "
+        #  i = i + 1
+        #end
+      
+      #end
+      #s = s + ")"
+      
+    
+
+    
   end
 
   def new
@@ -34,12 +78,14 @@ class MoviesController < ApplicationController
   end
   def sort
     redirect_to movies_path(:sort => params[:sort])
+    return
   end
 
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
+    return
   end
 
   def edit
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
     class << self
         def ratings
+     
             return ["G", "PG", "PG-13", "R"]
         end
     end
